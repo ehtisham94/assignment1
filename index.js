d31be41e3be39cf612a40f051e174481eaa0e853
ehtisham94 <ehtishammubashar94@gmail.com>
2022-03-17 08:16:36 +0500

    17-3-2022

--- index.js
+++ index.js
@@ -7,21 +7,7 @@ const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }));
 const createTable = require('./models/create')
-// createTable()
-
-// const insertedRows = await knex('students').insert({ first_name: 'abc', last_name: 'xyz' })
-//     console.log(1, insertedRows);
-
-// let getType = (req, res, next) => {
-//     let type = req.params._type
-//     if (type == 'student' || type == 'book') {
-//         req.body._type = type
-//         next()
-//     } else {
-//         res.json({status: 0, message: 'invalid url'})
-//     }
-// }
-// app.use('/:_type', getType, require('./routes/book'))
+createTable()
 
 app.use('/api/student', require('./routes/student'))
 app.use('/api/book', require('./routes/book'))
